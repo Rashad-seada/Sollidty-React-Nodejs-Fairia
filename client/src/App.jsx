8978bf6dc20268b27c8e98690af03df247ddb687
Fathy2050 <mo.fathy2050r@gmail.com>
2024-03-09 01:49:04 +0200

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+
+const App = () => {
+
+  return (
+    <html className = "App">
+          <link href="./output.css" rel="stylesheet"></link>
+
+ 
+      <body className="bg-black">
+        <h1 class="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </body>
+          
+     
+    </html>
+  )
+}
+
+export default App
