873e559107500512f7c051d9edc02e40de5be521
momen hesham <hmomen235@gmail.com>
2024-03-13 17:31:25 +0200

    [refactor] import TenderAppProvider in main

--- client/src/context/TenderAppContext.jsx
+++ client/src/context/TenderAppContext.jsx
@@ -1,29 +1,75 @@
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 
-import {contractABI , contractAddress } from '../utils/constants'
+import { contractABI, contractAddress } from "../utils/constants";
 
 export const TenderAppContext = React.createContext();
 
 const { ethereum } = window;
 
-
 const getEthereumContract = () => {
-    const provider = new ethers.providers.Web3Provider(ethereum);
-    const signer = provider.getSigner();
-    const tenderAppContract = new ethers.Contract(contractAddress,contractABI,signer)
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+  const tenderAppContract = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    signer
+  );
 
-    console.log({
-        provider,
-        signer,
-        tenderAppContract
-    })
-}
+  console.log({
+    provider,
+    signer,
+    tenderAppContract,
+  });
+};
 
-export const TenderAppProvider = ({ children }) => {
-    return (
-        <TenderAppContext.Provider>
-            { children }
-        </TenderAppContext.Provider>
-    )
-} 
+const TenderAppProvider = ({ children }) => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  //const [ allTenders ,setAllTenders] = useState([])
+
+  const checkIfTheWalletIsConnected = async () => {
+    if (!ethereum) return alert("Please install metamask");
+    const accounts = await ethereum.request({
+      method: "eth_accounts",
+    });
+  };
+
+  const connectWallet = async () => {
+    try {
+      if (!ethereum) return alert("Please install metamask");
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      throw new Error("no etherum object");
+    }
+  };
+
+  // const getAllTenders = async ()=> {
+  //     if (ethereum) {
+  //         const tenderAppContract = getEthereumContract();
+
+  //         const allTenders = await tenderAppContract.getTenders()
+  //         .then((tenders) => {
+  //             setAllTenders(tenders);
+  //         }).catch((error)=> {
+  //             throw new Error(error.message)
+  //         });
+  //     }
+  // }
+
+  useEffect(() => {
+    checkIfTheWalletIsConnected();
+    //getAllTenders();
+    // console.log(ethereum);
+  }, []);
+
+  return (
+    <TenderAppContext.Provider value={{ connectWallet }}>
+      {children}
+    </TenderAppContext.Provider>
+  );
+};
+
+export default TenderAppProvider;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import './input.css'
-import { TenderAppProvider } from './context/TenderAppContext.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import "./input.css";
+import TenderAppProvider from "./context/TenderAppContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <TenderAppProvider>
-      <React.StrictMode>
-          <BrowserRouter>
-              <App />
-          </BrowserRouter>
-      </React.StrictMode>
-  </TenderAppProvider>,
-  
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <TenderAppProvider>
+        <App />
+      </TenderAppProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
