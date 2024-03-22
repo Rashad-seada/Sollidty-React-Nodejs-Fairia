689a486d97a9f40eca361bf99ee45988d6952b5c
ibrahimshoeib <hsho2214@gmail.com>
2024-03-22 17:22:56 +0200

    feat : added search feature successfuly

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -7,9 +7,9 @@ import {TenderAppContext} from"../context/TenderAppContext"
 
 function SearchBar() {
   const {
-    searchTenders,tenders,setTenders
+    searchTenders,setTenders
   } = useContext(TenderAppContext);
-  
+
   const [search, setsearch] = useState("");
   function handelSearchValue(event) {
     setsearch(event.target.value);
@@ -24,7 +24,10 @@ function SearchBar() {
 
   return (
     <>
-      <form className="max-w-md mx-auto bg-primary py-20">
+      <form   
+      onSubmit={e => e.preventDefault()}
+       className="max-w-md mx-auto bg-primary py-20">
+
           <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
           <div className="relative">
               <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -38,7 +41,6 @@ function SearchBar() {
               type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-sky-400 border border-gray-300 rounded-3xl bg-secondary focus:ring-blue-500 focus:border-blue-500 dark:bg-primary dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
               <button 
               onClick={()=>onSubmit()}
-              type="submit"
                className="text-black absolute end-2.5 bottom-2.5 bg-sky-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-3xl text-sm px-4 py-2 dark:bg-ter dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
           </div>
       </form>
