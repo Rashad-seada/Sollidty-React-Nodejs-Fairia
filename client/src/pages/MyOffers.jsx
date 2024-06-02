6db999e1fbc44a9690b04f128b8d95e859bce46d
ibrahimshoeib <hsho2214@gmail.com>
2024-06-02 17:58:30 +0300

    chore

--- client/src/pages/MyOffers.jsx
+++ client/src/pages/MyOffers.jsx
@@ -1,13 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import NavBarV2 from '../components/NavBarV2'
+import Card from "../components/Card"
+import Footer from '../components/Footer'
 function MyOffers() {
+
   return (
     <div>
-              <NavBarV2 /> 
-
+              <NavBarV2 />
+              {Offer.map((tender, index) => (
+    <Card  key={index} title= {tender.title} description = {tender.description} id = {tender.id} bidBond = {tender.bidBond}/>
+))}
+<Footer />
       </div>
   )
 }
-
 export default MyOffers
---
+++ client/src/pages/Question.jsx
@@ -0,0 +1,32 @@
+// // eslint-disable-next-line no-unused-vars
+// import React from "react";
+
+// function Question() {
+//   return (
+//     <>
+//       <form className="main w-100% h-100vh bg-red-700 ">
+//         <div>
+//           <div className=" flex pb-2">
+//             <label>Question One</label>
+//           </div>
+//              <div className="flex items-center ">
+//                <div 
+//                style={{paddingLeft:"100px"}}>
+//                 <div>
+//                   <input type="radio" /> Option 1
+//                 </div>
+//                 <div>
+//                   <input type="radio" /> Option 2
+//                 </div>
+//                 <div>
+//                   <input type="radio" /> Option 3
+//                 </div>
+//                </div>
+//              </div>
+//          </div>
+//       </form>
+//     </>
+//   )
+// }
+
+// export default Question;
