46b26d7b37a56f745312e6982897bc2e453726ab
ibrahimshoeib <hsho2214@gmail.com>
2024-06-04 21:52:35 +0300

    Questions

--- client/src/components/Question.jsx
+++ client/src/components/Question.jsx
@@ -1,31 +1,42 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 
-function Question(QuestionOne,QuestionTwo,QuestionThree) {
+function Question() {
+        //Question,AnswerOne,AnswerTwo,AnswerThree
+
   return (
     <>
-      <form className="main w-100% h-100vh bg-red-700 ">
-        <div>
-          <div className=" flex pb-2">
-            <label>Question One</label>
-          </div>
-             <div className="flex items-center ">
-               <div 
-               style={{paddingLeft:"100px"}}>
-                <div>
-                  <input type="radio" /> {QuestionOne}
-                </div>
-                <div>
-                  <input type="radio" /> {QuestionTwo}
-                </div>
-                <div>
-                  <input type="radio" />{QuestionThree}
-                </div>
-               </div>
+    <form className="main w-100% h-100vh bg-red-700 ">
+      <div>
+        <div className=" flex pb-2">
+          <label>
+              {/* {Question} */}
+              Question
+              </label>
+        </div>
+           <div className="flex items-center ">
+             <div 
+             style={{paddingLeft:"100px"}}>
+              <div>
+                <input type="radio" /> 
+                k
+                {AnswerOne}
+              </div>
+              <div>
+                <input type="radio" /> 
+                {AnswerTwo}
+                l
+              </div>
+              <div>
+                <input type="radio" />
+                {AnswerThree}
+                k
+              </div>
              </div>
-         </div>
-      </form>
-    </>
+           </div>
+       </div>
+    </form>
+  </>
   )
 }
 
--- client/src/pages/Question.jsx
+++ client/src/pages/Question.jsx
@@ -1,37 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-
-function Questions(Question,AnswerOne,AnswerTwo,Answerthree) {
+import Question from "../components/Question";
+ 
+function Questions() {
+   const Questions = {Question.map((item, index) => (
+        <Question  key={index} AnswerOne= {item.AnswerOne} AnswerOne = {item.AnswerTwo} AnswerOne = {item.AnswerThree} />
+  ))}
   return (
-    <>
-      <form className="main w-100% h-100vh bg-red-700 ">
-        <div>
-          <div className=" flex pb-2">
-            <label>{Question}</label>
-          </div>
-             <div className="flex items-center ">
-               <div 
-               style={{paddingLeft:"100px"}}>
-                <div>
-                  <input type="radio" /> 
-                  k
-                  {/* {AnswerOne} */}
-                </div>
-                <div>
-                  <input type="radio" /> 
-                  {/* {AnswerTwo} */}
-                  l
-                </div>
-                <div>
-                  <input type="radio" />
-                  {/* {Answerthree} */}
-                  k
-                </div>
-               </div>
-             </div>
-         </div>
-      </form>
-    </>
+
+  {Questions}
   )
 }
 
