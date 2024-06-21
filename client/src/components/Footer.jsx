e7ea3333563ce279168cd694fccc2d7121d7b95a
ibrahimshoeib <hsho2214@gmail.com>
2024-06-22 02:47:55 +0300

    chore

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <section className="py-10 bg-secondary sm:pt-16 lg:pt-24">
+    <section className="py-10 bg-primary sm:pt-16 lg:pt-24 ">
     <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
         <div className="grid grid-cols-2 gap-x-5 gap-y-12 md:grid-cols-4 md:gap-x-12">
             <div>
@@ -8,16 +8,16 @@ const Footer = () => {
 
                 <ul className="mt-8 space-y-4">
                     <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> About </a>
+                        <a href="#" title="" className="text-base text-sky-400 transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> About </a>
                     </li>
                     <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Features </a>
+                        <a href="#" title="" className="text-base text-sky-400 transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Features </a>
                     </li>
                     <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Works </a>
+                        <a href="#" title="" className="text-base text-sky-400 transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Works </a>
                     </li>
                     <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Career </a>
+                        <a href="#" title="" className="text-base text-sky-400 transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Career </a>
                     </li>
                 </ul>
             </div>
@@ -27,16 +27,16 @@ const Footer = () => {
 
                 <ul className="mt-8 space-y-4">
                     <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Customer Support </a>
+                        <a href="#" title="" className="text-base text-sky-400 transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Customer Support </a>
                     </li>
                     <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Delivery Details </a>
+                        <a href="#" title="" className="text-base text-sky-400 transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Delivery Details </a>
                     </li>
                     <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Terms & Conditions </a>
+                        <a href="#" title="" className="text-base text-sky-400 transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Terms & Conditions </a>
                     </li>
                     <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Privacy Policy </a>
+                        <a href="#" title="" className="text-base text-sky-400 transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Privacy Policy </a>
                     </li>
                 </ul>
             </div>
@@ -46,16 +46,16 @@ const Footer = () => {
 
                 <ul className="mt-8 space-y-4">
                     <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Free eBooks </a>
+                        <a href="#" title="" className="text-base text-sky-400 transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Free eBooks </a>
                     </li>
                     <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Development Tutorial </a>
+                        <a href="#" title="" className="text-base text-sky-400 transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Development Tutorial </a>
                     </li>
                     <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> How to - Blog </a>
+                        <a href="#" title="" className="text-base text-sky-400 transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> How to - Blog </a>
                     </li>
                     <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> YouTube Playlist </a>
+                        <a href="#" title="" className="text-base text-sky-400 transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> YouTube Playlist </a>
                     </li>
                 </ul>
             </div>
@@ -65,23 +65,23 @@ const Footer = () => {
 
                 <ul className="mt-8 space-y-4">
                     <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Customer Support </a>
+                        <a href="#" title="" className="text-base text-sky-400 transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Customer Support </a>
                     </li>
                     <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Delivery Details </a>
+                        <a href="#" title="" className="text-base text-sky-400 transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Delivery Details </a>
                     </li>
                     <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Terms & Conditions </a>
+                        <a href="#" title="" className="text-base text-sky-400 transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Terms & Conditions </a>
                     </li>
                     <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Privacy Policy </a>
+                        <a href="#" title="" className="text-base text-sky-400 transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Privacy Policy </a>
                     </li>
                 </ul>
             </div>
         </div>
         <hr className="mt-16 mb-10 border-gray-800" />
         <div className="flex flex-wrap items-center justify-center">
-            <p className="w-full mt-8 text-sm text-center text-gray-100 md:mt-0 md:w-auto md:order-2">© Copyright 2024, All Rights Reserved by Codex Tech Company</p>
+            <p className="w-full mt-8 text-sm text-center text-gray-300 md:mt-0 md:w-auto md:order-2">© Copyright 2024, All Rights Reserved by Codex Tech Company</p>
         </div>
     </div>
 </section>
