8eb226e683255a571a37ba051866d076c38652ea
ibrahimshoeib <hsho2214@gmail.com>
2024-06-21 20:14:07 +0300

    chore

--- client/src/components/ApplicantsCard.jsx
+++ client/src/components/ApplicantsCard.jsx
@@ -8,47 +8,26 @@ function ApplicantCard({
    
   return (
     <>
-      {/* component */}
-      {/* This is an example component */}
+     
       <div className="flex flex-wrap  max-w-2xl ml-44 mt-20 bg-secon p-5 bg-secondary rounded-2xl">
         <div className="relative w-10 h-10 overflow-hidden bg-secondary rounded-full dark:bg-gray-600">
-
-          {/* <svg
-            className="absolute w-15 h-15 text-sky-400 -left-1"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-              clipRule="evenodd"
-            ></path>
-          </svg> */}
-
                     <img
                       src="https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"
                       className=" relative z-10 rounded-3xl"
                       alt=""
                     />
         </div>
-
-          
             <div className=" ">
             <div className=" pl-10">
                 <p className="pb-1 text-gray-600"> {applicant}</p>
 
                 <h2 className="pb-1 text-white text-xl tex"> {form} </h2>
-              
             </div>
-         
            <div className=" pt-4 pl-0">
            <h1 className="pb-1 text-white"> {title} </h1>
              <p className=" text-gray-400"> {description} </p>
            </div>
         </div>
-
-
       </div>
     </>
   );
--- client/src/components/OfferCard.jsx
+++ client/src/components/OfferCard.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
+
 function OfferCard({title,description} ) {
   
   // const {id} = useParams ();
@@ -10,122 +11,27 @@ function OfferCard({title,description} ) {
     <>
       <div className='bg-primary'>
  <div style={{fontSize:"80px"}}>
-          <p className=" font-semibold flex justify-center text-cyan-50   py-36"> My<span className="text-sky-400 px-3">Offers</span> </p>
+          <p className=" font-semibold flex justify-center text-cyan-50   py-14"> My<span className="text-sky-400 px-3">Offers</span> </p>
         </div>
-
- <div className="flex  items-center justify-center flex-wrap pb-14 ">
-          <div 
-          style={{height:"350px"}}
-          className="relative flex max-w-[80%]  p-200 flex-row rounded-xl bg-secondary bg-clip-border text-gray-700 shadow-md">
-            <div className="relative m-0  p-0 w-2/5 shrink-0 overflow-hidden rounded-xl rounded-r-none bg-white bg-clip-border text-gray-700">
-              <img
-                src="https://freedesignfile.com/upload/2016/06/Simple-blueprint-building-vectors-design-05.jpg"
-                alt="image"
-                className="h-full w-full object-cover"
-              />
-            </div>
-            <div className="p-6">
-             
-              <h4 className="mb-2 block pb-9 pt-10 font-sans text-3xl font-semibold leading-snug tracking-normal text-sky-400 antialiased">
-                Lyft launching cross-platform service this week
-                {/* {title} */}
-              </h4>
-              <p className="mb-4 block font-sans pb-5 text-base font-normal leading-relaxed text-gray-50 antialiased">
-                Like so many organizations these days, Autodesk is a company in
-                transition. It was until recently a traditional boxed software
-                company selling licenses. Yet its own business model disruption
-                is only part of the story
-                {/* {description} */}
-              </p>
-              <a className="flex justify-center  pl-96" href="#">
-                <Link
-                  to={`/MyOffers`}
-                  className="inline-flex items-center w-40  p-3 text-xl font-medium text-center text-black bg-ter rounded-lg hover:bg-ter focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-ter dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Read more 
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={2}
-                    stroke="currentColor"
-                    aria-hidden="true"
-                    className="h-4 w-4 p-1 size-10 "
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+     <div className=' pb-44  pr-20 w-70% '>
+      <div className="flex   ml-44 mt-20 bg-secon p-10  pb-24  bg-secondary rounded-2xl">
+        <div className=" text-3xl  relative w-20  h-20 overflow-hidden bg-secondary rounded-full ">
+                    <img
+                      src="https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"
+                      className=" relative z-10 rounded-sm bg-sky-400 "
+                      alt="Person-logo"
                     />
-                  </svg>
-                </Link>
-              </a>
+        </div>
+            <div >
+            <div className=" pl-20">
             </div>
-          </div>
-          <link
-            rel="stylesheet"
-            href="https://unpkg.com/@material-tailwind/html@latest/styles/material-tailwind.css"
-          />
-
-
-
-
-</div>
- <div className="flex  items-center justify-center flex-wrap pb-14 ">
-          <div 
-          style={{height:"350px"}}
-          className="relative flex max-w-[80%]  p-200 flex-row rounded-xl bg-secondary bg-clip-border text-gray-700 shadow-md">
-            <div className="relative m-0  p-0 w-2/5 shrink-0 overflow-hidden rounded-xl rounded-r-none bg-white bg-clip-border text-gray-700">
-              <img
-                src="https://freedesignfile.com/upload/2016/06/Simple-blueprint-building-vectors-design-05.jpg"
-                alt="image"
-                className="h-full w-full object-cover"
-              />
-            </div>
-            <div className="p-6">
-             
-              <h4 className="mb-2 block pb-9 pt-10 font-sans text-3xl font-semibold leading-snug tracking-normal text-sky-400 antialiased">
-                {title}
-              </h4>
-              <p className="mb-4 block font-sans pb-5 text-base font-normal leading-relaxed text-gray-50 antialiased">
-                
-                {description}
-              </p>
-              <a className="flex justify-center  pl-96" href="#">
-                <Link
-                  to={`/MyOffers`}
-                  className="inline-flex items-center w-40  p-3 text-xl font-medium text-center text-black bg-ter rounded-lg hover:bg-ter focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-ter dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Read more 
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={2}
-                    stroke="currentColor"
-                    aria-hidden="true"
-                    className="h-4 w-4 p-1 size-10 "
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-                    />
-                  </svg>
-                </Link>
-              </a>
-            </div>
-          </div>
-          <link
-            rel="stylesheet"
-            href="https://unpkg.com/@material-tailwind/html@latest/styles/material-tailwind.css"
-          />
-
-
-
-
-</div>
-
+           <div className=" pt-4 ">
+           <h1 className="pb-1 text-sky-400 pl-28 text-3xl"> {title} </h1>
+             <p className=" text-gray-400 pt-4 pl-32"> {description} </p>
+           </div>
+        </div>
+      </div>
+     </div>
 </div>
 </>
   )
