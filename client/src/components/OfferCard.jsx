a1d879ea2fa1e6192560e15b61eb3202291738d8
Fathy2050 <mo.fathy2050r@gmail.com>
2024-06-22 02:34:11 +0300

    fix : fixed the offer card

--- client/src/components/OfferCard.jsx
+++ client/src/components/OfferCard.jsx
@@ -11,8 +11,8 @@ function OfferCard({title,description} ) {
     <>
       <div className='bg-primary'>
 
-     <div className=' pb-44  pr-20 w-70% '>
-      <div className="flex   ml-44 mt-20 bg-secon p-10  pb-24  bg-secondary rounded-2xl">
+     <div className='  w-70% '>
+      <div className="flex  p-11 mb-11  bg-secondary rounded-2xl">
         <div className=" text-3xl  relative w-20  h-20 overflow-hidden bg-secondary rounded-full ">
                     <img
                       src="https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"
@@ -23,9 +23,9 @@ function OfferCard({title,description} ) {
             <div >
             <div className=" pl-20">
             </div>
-           <div className=" pt-4 ">
-           <h1 className="pb-1 text-sky-400 pl-28 text-3xl"> {title} </h1>
-             <p className=" text-gray-400 pt-4 pl-32"> {description} </p>
+           <div className=" ">
+           <h1 className=" text-sky-400 pl-11 text-3xl"> {title} </h1>
+             <p className=" text-gray-400 pt-4 pl-11"> {description} </p>
            </div>
         </div>
       </div>
