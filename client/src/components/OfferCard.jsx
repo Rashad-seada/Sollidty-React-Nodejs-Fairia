56cc4c266ea1f691eda300d13f8c823a1a7a29b1
Fathy2050 <mo.fathy2050r@gmail.com>
2024-06-22 03:00:36 +0300

    fix  :

--- client/src/components/OfferCard.jsx
+++ client/src/components/OfferCard.jsx
@@ -11,15 +11,15 @@ function OfferCard({title,description} ) {
     <>
       <div className='bg-primary'>
 
-     <div className='  w-70% '>
-      <div className="flex  p-11 mb-11  bg-secondary rounded-2xl">
-        <div className=" text-3xl  relative w-20  h-20 overflow-hidden bg-secondary rounded-full ">
-                    <img
-                      src="https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"
-                      className=" relative z-10 rounded-sm bg-sky-400 "
-                      alt="Person-logo"
-                    />
-        </div>
+     <div className='   '> 
+      <div className="w-70% flex  p-11 mb-11  bg-secondary rounded-2xl">
+          <div className=" text-3xl  relative w-20  h-20 overflow-hidden bg-secondary rounded-full ">
+                      <img
+                        src="https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"
+                        className=" relative z-10 rounded-sm bg-sky-400 "
+                        alt="Person-logo"
+                      />
+          </div>
 
             <div >
             <div className=" pl-20">
